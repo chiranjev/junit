1724e54af3c913b1049c9529d9dac2fc696e4ce1
Chiranjev Koul <ckkoul@amazon.com>
2021-12-27 21:34:59 +0530

    basic assert method

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -6,7 +6,28 @@ import static org.junit.Assert.*;
 
 public class StringHelperTest {
     @Test
-    public void test() {
-        fail("Not yet impmlemented");
+    public void test(){
+//        fail("Not yet impmlemented"); // failed
+//        assertEquals("ABC","ABCD");   // failed
+//        assertEquals("ABC","ABC");    // passed
     }
+    StringHelper helper = new StringHelper();
+
+    @Test
+    public void testTruncateAInFirst2Positions_WhenAInFirst2Positions() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_WhenAInFirst1Position() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_WhenANotInFirst2Position() {
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+    }
+
+
+
 }
